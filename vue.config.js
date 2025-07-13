5b9b7e5ecfde6f3d44ba419185622138e1e452cc
Joello61 <tchindajoel61@gmail.com>
2025-07-13 10:52:58 +0200

    Ajouter la gestion des pages dans la configuration de Vue et inclure le fichier .env dans .gitignore

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,16 @@ const webpack = require('webpack')
 
 module.exports = defineConfig({
   transpileDependencies: true,
+
+  pages: {
+    index: {
+      entry: 'src/main.js',              // ou main.ts si TypeScript
+      template: 'public/index.html',
+      filename: 'index.html',
+      title: 'Portfolio Joeltech',       // 🎯 ICI ton titre
+    }
+  },
+
   configureWebpack: {
     plugins: [
       new webpack.DefinePlugin({
